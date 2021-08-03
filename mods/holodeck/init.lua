90c23b91db97e27fe0eb9f495c8e6cfa28123135
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-08-03 15:39:41 +0200

    moar planets

--- mods/holodeck/init.lua
+++ mods/holodeck/init.lua
@@ -8,6 +8,14 @@ lua_trigger.register_trigger("origin_holodeck", function(pos)
     end
 
     map = starmap.new(pos)
+
+    map:add_planet({x=0, y=2, z=0}, {
+        texture = "default_lava.png",
+        size = 2,
+        automatic_rotate = 0.1,
+        glow = 14,
+    })
+
     map:add_ring({x=0, y=2, z=0}, {
         radius = 4
     })
@@ -17,7 +25,7 @@ lua_trigger.register_trigger("origin_holodeck", function(pos)
         color = "#00FF00"
     })
 
-    local txt = map:add_text({x=0, y=2, z=-5}, {
+    local txt = map:add_text({x=0, y=4, z=-5}, {
         text = "Select a planet",
         color = "#0000FF",
         glow = 14
@@ -35,6 +43,25 @@ lua_trigger.register_trigger("origin_holodeck", function(pos)
             txt:update()
         end
     })
+
+    map:add_ring({x=0, y=2, z=5}, {
+        radius = 2,
+        color = "#0000FF"
+    })
+
+    map:add_planet({x=2, y=2, z=5}, {
+        texture = "scifi_nodes_grey_square.png",
+        size = 0.5,
+        automatic_rotate = 1,
+        name = "Unnamed station",
+        glow = 14,
+        on_punch = function()
+            txt:set_text("Selected:\nUnnamed station")
+            txt:set_color("#0000FF")
+            txt:update()
+        end
+    })
+
     map:add_planet({x=4, y=2, z=0}, {
         texture = "default_sand.png",
         size = 1.5,
