1fb85d7a4cd66dc9df5f6bebd98de4606a08b48b
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-08-09 09:05:03 +0200

    cached ship status

--- mods/stellar/ship.lua
+++ mods/stellar/ship.lua
@@ -1,7 +1,14 @@
 
+local status
+
 function stellar.get_ship_status()
+    -- cached
+    if status then
+        return status
+    end
+
+    -- read from storage
     local status_str = stellar.storage:get_string("ship")
-    local status
     if status_str then
         status = minetest.parse_json(status_str)
     end
@@ -20,7 +27,8 @@ function stellar.get_ship_status()
     return status
 end
 
-function stellar.set_ship_status(status)
+function stellar.set_ship_status(new_status)
+    status = new_status
     local json = minetest.write_json(status)
     stellar.storage:set_string("ship", json)
 end
