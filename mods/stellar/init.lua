6ac47c93ccad73a14aa5c18427c2c2be5072aa6a
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-08-09 14:26:32 +0200

    merge into one mod

--- mods/stellar/init.lua
+++ mods/stellar/init.lua
@@ -5,5 +5,8 @@ stellar = {
 local MP = minetest.get_modpath("stellar")
 dofile(MP.."/system.lua")
 dofile(MP.."/ship.lua")
+dofile(MP.."/teleporter.lua")
 dofile(MP.."/skybox.lua")
+dofile(MP.."/systems/helios.lua")
+
 dofile(MP.."/test.lua")
--- mods/stellar_data/init.lua
+++
@@ -1,4 +0,0 @@
-
-local MP = minetest.get_modpath("stellar_data")
-dofile(MP.."/teleporter.lua")
-dofile(MP.."/systems/helios.lua")
