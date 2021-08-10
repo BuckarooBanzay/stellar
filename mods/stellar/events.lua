7fe89ed36f200c508477a039eca7f5130e7d123b
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-08-10 10:35:59 +0200

    eventemitter

--- mods/stellar/events.lua
+++ mods/stellar/events.lua
@@ -1,38 +1,40 @@
 
--- name -> table {}
-local handlers = {}
+local function EventEmitter()
+    -- name -> table {}
+    local handlers = {}
 
-local function get_list(name)
-    local list = handlers[name]
-    if not list then
-        list = {}
-        handlers[name] = list
-    end
-
-    return list
-end
-
-stellar.events = {}
-
-function stellar.events.on(name, fn)
-    local list = get_list(name)
-    table.insert(list, fn)
-end
-
-function stellar.events.off(name, fn)
-    local list = get_list(name)
-    local new_list = {}
-    for _, existing_fn in ipairs(list) do
-        if existing_fn ~= fn then
-            table.insert(new_list, existing_fn)
+    local function get_list(name)
+        local list = handlers[name]
+        if not list then
+            list = {}
+            handlers[name] = list
         end
+
+        return list
     end
-    handlers[name] = list
+
+    return {
+        on = function(name, fn)
+            local list = get_list(name)
+            table.insert(list, fn)
+        end,
+        off = function(name, fn)
+            local list = get_list(name)
+            local new_list = {}
+            for _, existing_fn in ipairs(list) do
+                if existing_fn ~= fn then
+                    table.insert(new_list, existing_fn)
+                end
+            end
+            handlers[name] = list
+        end,
+        emit = function(name, data)
+            local list = get_list(name)
+            for _, fn in ipairs(list) do
+                fn(data)
+            end
+        end
+    }
 end
 
-function stellar.events.emit(name, data)
-    local list = get_list(name)
-    for _, fn in ipairs(list) do
-        fn(data)
-    end
-end
+stellar.events = EventEmitter()
