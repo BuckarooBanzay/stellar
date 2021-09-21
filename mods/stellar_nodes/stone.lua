b8260f00d48981bfc0b51da46ef2509ce6836907
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-09-21 10:36:26 +0200

    water stuff test

--- mods/stellar_nodes/stone.lua
+++ mods/stellar_nodes/stone.lua
@@ -35,3 +35,93 @@ minetest.register_node("stellar_nodes:dirt_with_grass2", {
     },
     groups = {crumbly=1, oddly_breakable_by_hand=1, soil=1}
 })
+
+
+minetest.register_node("stellar_nodes:water_source", {
+	description = S("Water Source"),
+	drawtype = "liquid",
+	waving = 3,
+	tiles = {
+		{
+			name = "stellar_nodes_water_source_animated.png^[colorize:#AA0000:50",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 2.0,
+			},
+		},
+		{
+			name = "stellar_nodes_water_source_animated.png^[colorize:#AA0000:50",
+			backface_culling = true,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 2.0,
+			},
+		},
+	},
+	use_texture_alpha = "blend",
+	paramtype = "light",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	drop = "",
+	drowning = 1,
+	liquidtype = "source",
+	liquid_alternative_flowing = "stellar_nodes:water_flowing",
+	liquid_alternative_source = "stellar_nodes:water_source",
+	liquid_viscosity = 1,
+	post_effect_color = {a = 103, r = 30, g = 60, b = 90},
+	groups = {water = 3, liquid = 3, cools_lava = 1},
+})
+
+minetest.register_node("stellar_nodes:water_flowing", {
+	description = S("Flowing Water"),
+	drawtype = "flowingliquid",
+	waving = 3,
+	tiles = {"stellar_nodes_water.png"},
+	special_tiles = {
+		{
+			name = "stellar_nodes_water_flowing_animated.png^[colorize:#AA0000:50",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 0.5,
+			},
+		},
+		{
+			name = "stellar_nodes_water_flowing_animated.png^[colorize:#AA0000:50",
+			backface_culling = true,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 0.5,
+			},
+		},
+	},
+	use_texture_alpha = "blend",
+	paramtype = "light",
+	paramtype2 = "flowingliquid",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	drop = "",
+	drowning = 1,
+	liquidtype = "flowing",
+	liquid_alternative_flowing = "stellar_nodes:water_flowing",
+	liquid_alternative_source = "stellar_nodes:water_source",
+	liquid_viscosity = 1,
+	post_effect_color = {a = 103, r = 30, g = 60, b = 90},
+	groups = {water = 3, liquid = 3, not_in_creative_inventory = 1,
+		cools_lava = 1},
+})
